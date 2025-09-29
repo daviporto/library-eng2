04627e59e82624304856f6c2a9183800958f1a59
davi <davi.araujo@onfly.com.br>
2025-09-29 20:09:03 -0300

    Commit 10: Replacing conditional with polymorphism - part 3.

--- model.py
+++ model.py
@@ -32,13 +32,30 @@ class Price:
         pass
 
 class RegulaPrice(Price):
-    pass
+    def get_charge(self, days_rented: int) -> float:
+        amount = 2
+        if days_rented > 2:  amount += (days_rented - 2) * 1.5
+        return amount
+
+    def get_frequent_renter_points(self, days_rented):return 1
+
 
 class NewReleasePrice(Price):
-    pass
+    def get_charge(self, days_rented: int) -> float:
+        return days_rented * 3
+
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        points = 1
+        if days_rented > 1:    points += 1
+        return points
 
 class ChildrenPrice(Price):
-    pass
+    def get_charge(self, days_rented: int) -> float:
+        amount = 1.5
+        if days_rented > 3: amount += (days_rented - 3) * 1.5
+        return amount
+
+    def get_frequent_renter_points(self, days_rented):  return 1
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
@@ -51,8 +68,8 @@ class Rental:
     def days_rented(self) -> int:
         return self.days_rented
 
-    def get_frequent_renter_points(self, points):
-        return self.book.get_frequent_renter_points(points, self.days_rented)
+    def get_frequent_renter_points(self):
+        return self.book.get_frequent_renter_points(self.days_rented)
 
     def get_charge(self) -> float:
        return self.book.get_charge(self.days_rented)
@@ -78,7 +95,7 @@ class Client:
             amount = rental.get_charge()
 
             # add frequent renter points
-            frequent_renter_points = rental.get_frequent_renter_points(frequent_renter_points)
+            frequent_renter_points += rental.get_frequent_renter_points()
 
             # show each rental result
             result += f"- {rental.book.title}: {amount}\n"
